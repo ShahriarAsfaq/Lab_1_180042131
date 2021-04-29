38a411a3a6ae230adcf4ccfc81459a8e22c0a27a
Shahriar Asfaq <shahriarasfaq@iut-dhaka.edu>
2021-04-30 00:13:29 +0600

    completed globalvariables module

---
+++ globalvariables.js
@@ -0,0 +1,11 @@
+const dictoryname = __dirname ;
+console.log(dictoryname);
+
+console.log(__filename);
+
+const fs = require("fs");
+console.log(fs);
+
+console.log(module);
+
+console.log(process);
