3eab19192d1460fed2c3ba9508f35ae24179bdea
Shahriar Asfaq <shahriarasfaq@iut-dhaka.edu>
2021-04-30 14:24:34 +0600

    app bug fix

--- loadContent.js
+++ loadContent.js
@@ -1,66 +1,17 @@
 const fs= require("fs");
 
-const base=fs.readFile("./HTML/index.html","utf-8",(err,data) =>{
-    if (err){
-        console.log(err);
-    }
-    else{
-        console.log(data);
-    }
-    });
+const base=fs.readFileSync("./HTML/index.html","utf-8");
 
-const blog=fs.readFile("./HTML/blog.html","utf-8",(err,data) =>{
-    if (err){
-            console.log(err);
-     }
-    else{
-            console.log(data);
-    }
-    });
+const blog=fs.readFileSync("./HTML/blog.html","utf-8");
 
-    const about=fs.readFile("./HTML/about.html","utf-8",(err,data) =>{
-        if (err){
-            console.log(err);
-        }
-        else{
-            console.log(data);
-        }
-        });
+    const about=fs.readFileSync("./HTML/about.html","utf-8");
     
-        const contact=fs.readFile("./HTML/contact.html","utf-8",(err,data) =>{
-            if (err){
-                console.log(err);
-            }
-            else{
-                console.log(data);
-            }
-            });
+        const contact=fs.readFileSync("./HTML/contact.html","utf-8");
 
-            const pricing=fs.readFile("./HTML/pricing.html","utf-8",(err,data) =>{
-                if (err){
-                    console.log(err);
-                }
-                else{
-                    console.log(data);
-                }
-                });
+            const pricing=fs.readFileSync("./HTML/pricing.html","utf-8");
 
-                const services=fs.readFile("./HTML/services.html","utf-8",(err,data) =>{
-                    if (err){
-                        console.log(err);
-                    }
-                    else{
-                        console.log(data);
-                    }
-                    });
+                const services=fs.readFileSync("./HTML/services.html","utf-8");
 
-                    const work=fs.readFile("./HTML/work.html","utf-8",(err,data) =>{
-                        if (err){
-                            console.log(err);
-                        }
-                        else{
-                            console.log(data);
-                        }
-                        });
+                    const work=fs.readFileSync("./HTML/work.html","utf-8");
 
                         module.exports={about,blog,contact,base,pricing,services,work};
